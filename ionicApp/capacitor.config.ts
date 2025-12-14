0a84b824ed3c8cba4a2bdf95277eb47e1e2fec96
Ariel <garciamatutehector8@gmail.com>
2025-12-13 20:37:34 -0500

    version mas estable y actualizada con ionic y android + readme y correccion de android con apartado de conexion local

--- ionicApp/capacitor.config.ts
+++ ionicApp/capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.singleseven.betpro',
   appName: 'BetPro',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    cleartext: true,
+    androidScheme: 'http'
+  },
+  android: {
+    allowMixedContent: true
+  }
 };
 
 export default config;
